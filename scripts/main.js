c7ccb44170c8b58af3a18e52b062ab38d0eb8d63
Rap1zer <125188052+Rap1zer@users.noreply.github.com>
2023-06-04 11:42:00 +0100

    testing around

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,20 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
 const createWindow = () => {
-   const win = new BrowserWindow({
-     width: 800,
-     height: 600
-   })
- 
-   win.loadFile('./scripts/index.html')
- }
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
 
- app.whenReady().then(() => {
-   createWindow()
- })
+  win.loadFile("./scripts/index.html");
+};
+
+app.whenReady().then(() => {
+  mainWindow = createWindow();
+});
+
+//On OS X it is common for the app to stay active even when windows are closed
+app.on("window-all-closed", () => {
+  if (process.platform != "darwin") {
+    app.quit();
+  }
+});
