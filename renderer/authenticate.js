c7d77e2767d10d906951f4d5bba7b2bb1aa0420f
Rap1zer <125188052+Rap1zer@users.noreply.github.com>
2023-07-04 15:48:25 +0100

    created modal html and css

--- renderer/authenticate.js
+++ renderer/authenticate.js
@@ -2,12 +2,11 @@ const loginBtn = document.getElementById("login-btn");
 const passwordInput = document.getElementById("password-check");
 
 loginBtn.addEventListener("click", async () => {
-  const validAuthentication = await window.api.checkMasterPass(
-    passwordInput.value
-  );
+  const passwordsMatch = await window.api.checkMasterPass(passwordInput.value);
 
-  if (validAuthentication) {
+  if (passwordsMatch) {
     console.log("passwords match");
+    window.location.href = "main-page.html";
   } else {
     console.log("passwords don't match");
   }
