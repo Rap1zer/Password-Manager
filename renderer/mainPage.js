fa16902d7c3f2e5d66d347a6b6a6a623ac8e68cf
Rap1zer <125188052+Rap1zer@users.noreply.github.com>
2023-08-03 13:36:48 +0100

    Record side bar now works partially

--- renderer/mainPage.js
+++ renderer/mainPage.js
@@ -1,7 +1,16 @@
+const recordsSideBar = document.getElementById("side-records");
+
 (async () => {
   const records = await window.api.getRecords();
   console.log(records);
-})();
-// for (let i = 0; i < ) {
 
-// }
+  for (let i = 0; i < records.length; i++) {
+    recordsSideBar.innerHTML += `<button class="record">
+      <img class="record-icon" src="../images/unknown-logo.svg" alt="" />
+      <div class="record-info">
+        <h1 class="record-title">${records[i].title}</h1>
+        <p class="record-description">${records[i].description}</p>
+      </div>
+    </button>`;
+  }
+})();
