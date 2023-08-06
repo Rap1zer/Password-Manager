44eb4906b39aef579b21674cd8c8b41a9637c99f
Rap1zer <125188052+Rap1zer@users.noreply.github.com>
2023-08-06 23:40:34 +0100

    added ability to highlight various buttons
    Delete folder button also goes invisible when a folder button is not highlighted.

--- renderer/mainPage.js
+++ renderer/mainPage.js
@@ -2,7 +2,9 @@ const recordsSideBar = document.getElementById("side-records");
 const sideMenu = document.getElementById("side-menu");
 const folderSection = document.getElementById("folders-section");
 const createFolderBtn = document.getElementById("create-folder-btn");
-let oldSelectedFolder;
+const deleteFolderBtn = document.getElementById("delete-folder-btn");
+const everythingBtn = document.getElementById("everything-btn");
+const starredBtn = document.getElementById("starred-btn");
 
 (async () => {
   // Wait for list of saved password records
@@ -20,35 +22,52 @@ let oldSelectedFolder;
       </div>
     </button>`;
   }
+})();
 
-  // load folders onto the folders side menu
-  for (let i = 0; i < records.length; i++) {}
+// load folders onto the folders side menu
+//for (let i = 0; i < folders.length; i++) {}
 
-  // Event listener for the create a folder button
-  createFolderBtn.addEventListener("click", () => {
-    folderSection.innerHTML += `
-    <a class="folder-btn unselectable new-folder button-selected">
+// Event listener for the create a folder button
+createFolderBtn.addEventListener("click", () => {
+  folderSection.innerHTML += `
+    <a class="folder-btn unselectable new-folder">
       <input type="text" id="new-folder">
     </a>`;
 
-    changeSelectedBtn(folderSection.lastChild);
+  changeSelectedBtn(folderSection.lastChild);
 
-    // Reference to the text input in the new folder element
-    const newFolderInput = document.getElementById("new-folder");
-    newFolderInput.focus();
+  // Reference to the text input in the new folder element
+  const newFolderInput = document.getElementById("new-folder");
+  newFolderInput.focus();
 
-    // Event listener that fires after the enter key is pressed or the input is no longer focused
-    newFolderInput.addEventListener("focusout", () => {
-      createNewFolder(newFolderInput.value);
-    });
-    newFolderInput.addEventListener("keypress", (e) => {
-      if (e.key === "Enter") createNewFolder(newFolderInput.value);
-    });
+  // Event listener that fires after the enter key is pressed or the input is no longer focused
+  newFolderInput.addEventListener("focusout", () => {
+    createNewFolder(newFolderInput.value);
   });
-})();
+  newFolderInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") createNewFolder(newFolderInput.value);
+  });
+});
+
+everythingBtn.addEventListener("click", (e) => {
+  changeSelectedBtn(everythingBtn);
+});
+
+starredBtn.addEventListener("click", (e) => {
+  changeSelectedBtn(starredBtn);
+});
+
+deleteFolderBtn.addEventListener("click", (e) => {
+  const selectedFolder =
+    folderSection.getElementsByClassName("button-selected")[0];
+
+  if (selectedFolder) {
+    selectedFolder.remove();
+  }
+});
 
 folderSection.addEventListener("click", (e) => {
-  if (e.target !== folderSection) {
+  if (e.target.classList.contains("folder-btn")) {
     changeSelectedBtn(e.target);
   }
 });
@@ -65,10 +84,16 @@ function createNewFolder(newFolderName) {
 }
 
 function changeSelectedBtn(newSelectedBtn) {
-  const selectedButtons = document.getElementsByClassName("button-selected");
+  const selectedButtons = sideMenu.getElementsByClassName("button-selected");
   for (btn of selectedButtons) {
     if (btn.classList.contains("button-selected"))
       btn.classList.remove("button-selected");
+  }
+
+  if (newSelectedBtn.classList.contains("folder-btn")) {
+    deleteFolderBtn.style.visibility = "visible";
+  } else {
+    deleteFolderBtn.style.visibility = "hidden";
   }
 
   newSelectedBtn.classList.add("button-selected");
